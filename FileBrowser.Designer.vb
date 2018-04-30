9a730c8d68308ca08fe8d5870760d0d077c8477f
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2018-05-01 01:21:04 +0200

    Renamed form, added basic functionality thanks to https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/creating-an-explorer-style-interface-with-the-listview-and-treeview
    Also fixed incorrect app.manifest encoding causing the app to give a Win32 error on launch

--- FileBrowser.Designer.vb
+++ FileBrowser.Designer.vb
@@ -1,12 +1,4 @@
-﻿'
-' Created by SharpDevelop.
-' User: Walkman
-' Date: 2018/04/28
-' Time: 23:48
-' 
-' To change this template use Tools | Options | Coding | Edit Standard Headers.
-'
-Partial Class MainForm
+﻿Partial Class FileBrowser
     Inherits System.Windows.Forms.Form
     
     ''' <summary>
@@ -33,11 +25,105 @@ Partial Class MainForm
     ''' not be able to load this method if it was changed manually.
     ''' </summary>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FileBrowser))
+        Me.splitContainerMain = New System.Windows.Forms.SplitContainer()
+        Me.treeViewFolders = New System.Windows.Forms.TreeView()
+        Me.imageListMain = New System.Windows.Forms.ImageList(Me.components)
+        Me.listViewFiles = New System.Windows.Forms.ListView()
+        Me.columnHeaderName = New System.Windows.Forms.ColumnHeader()
+        Me.columnHeaderType = New System.Windows.Forms.ColumnHeader()
+        Me.columnHeaderLastModified = New System.Windows.Forms.ColumnHeader()
+        CType(Me.splitContainerMain,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.splitContainerMain.Panel1.SuspendLayout
+        Me.splitContainerMain.Panel2.SuspendLayout
+        Me.splitContainerMain.SuspendLayout
+        Me.SuspendLayout
         '
-        'MainForm
+        'splitContainerMain
         '
+        Me.splitContainerMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.splitContainerMain.Location = New System.Drawing.Point(0, 0)
+        Me.splitContainerMain.Name = "splitContainerMain"
+        '
+        'splitContainerMain.Panel1
+        '
+        Me.splitContainerMain.Panel1.Controls.Add(Me.treeViewFolders)
+        '
+        'splitContainerMain.Panel2
+        '
+        Me.splitContainerMain.Panel2.Controls.Add(Me.listViewFiles)
+        Me.splitContainerMain.Size = New System.Drawing.Size(916, 580)
+        Me.splitContainerMain.SplitterDistance = 305
+        Me.splitContainerMain.TabIndex = 0
+        '
+        'treeViewFolders
+        '
+        Me.treeViewFolders.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.treeViewFolders.ImageIndex = 0
+        Me.treeViewFolders.ImageList = Me.imageListMain
+        Me.treeViewFolders.Location = New System.Drawing.Point(0, 0)
+        Me.treeViewFolders.Name = "treeViewFolders"
+        Me.treeViewFolders.SelectedImageIndex = 0
+        Me.treeViewFolders.Size = New System.Drawing.Size(305, 580)
+        Me.treeViewFolders.TabIndex = 0
+        '
+        'imageListMain
+        '
+        Me.imageListMain.ImageStream = CType(resources.GetObject("imageListMain.ImageStream"),System.Windows.Forms.ImageListStreamer)
+        Me.imageListMain.TransparentColor = System.Drawing.Color.Transparent
+        Me.imageListMain.Images.SetKeyName(0, "imageres(#3)-Folder.ico")
+        Me.imageListMain.Images.SetKeyName(1, "format-justify-center.png")
+        '
+        'listViewFiles
+        '
+        Me.listViewFiles.AllowColumnReorder = true
+        Me.listViewFiles.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.columnHeaderName, Me.columnHeaderType, Me.columnHeaderLastModified})
+        Me.listViewFiles.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.listViewFiles.FullRowSelect = true
+        Me.listViewFiles.GridLines = true
+        Me.listViewFiles.HideSelection = false
+        Me.listViewFiles.LabelEdit = true
+        Me.listViewFiles.Location = New System.Drawing.Point(0, 0)
+        Me.listViewFiles.Name = "listViewFiles"
+        Me.listViewFiles.Size = New System.Drawing.Size(607, 580)
+        Me.listViewFiles.SmallImageList = Me.imageListMain
+        Me.listViewFiles.TabIndex = 0
+        Me.listViewFiles.UseCompatibleStateImageBehavior = false
+        Me.listViewFiles.View = System.Windows.Forms.View.Details
+        '
+        'columnHeaderName
+        '
+        Me.columnHeaderName.Text = "Name"
+        '
+        'columnHeaderType
+        '
+        Me.columnHeaderType.Text = "Type"
+        '
+        'columnHeaderLastModified
+        '
+        Me.columnHeaderLastModified.Text = "Last Modified"
+        '
+        'FileBrowser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Name = "MainForm"
+        Me.ClientSize = New System.Drawing.Size(916, 580)
+        Me.Controls.Add(Me.splitContainerMain)
+        Me.Name = "FileBrowser"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "FileBrowser"
+        Me.splitContainerMain.Panel1.ResumeLayout(false)
+        Me.splitContainerMain.Panel2.ResumeLayout(false)
+        CType(Me.splitContainerMain,System.ComponentModel.ISupportInitialize).EndInit
+        Me.splitContainerMain.ResumeLayout(false)
+        Me.ResumeLayout(false)
     End Sub
+    Private WithEvents columnHeaderLastModified As System.Windows.Forms.ColumnHeader
+    Private WithEvents columnHeaderType As System.Windows.Forms.ColumnHeader
+    Private WithEvents columnHeaderName As System.Windows.Forms.ColumnHeader
+    Private WithEvents listViewFiles As System.Windows.Forms.ListView
+    Private WithEvents treeViewFolders As System.Windows.Forms.TreeView
+    Private imageListMain As System.Windows.Forms.ImageList
+    Private splitContainerMain As System.Windows.Forms.SplitContainer
 End Class
--- FileBrowser.vb
+++ FileBrowser.vb
@@ -1,10 +1,92 @@
-﻿Public Partial Class MainForm
+﻿Imports System.IO
+Public Partial Class FileBrowser
     Public Sub New()
         ' The Me.InitializeComponent call is required for Windows Forms designer support.
         Me.InitializeComponent()
         
-        '
-        ' TODO : Add constructor code after InitializeComponents
-        '
+    End Sub
+    
+    Sub FileBrowser_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        'PopulateTreeView("B:\")
+        PopulateTreeView("..\..")
+'        For Each item In System.IO.DriveInfo.GetDrives()
+'            Try
+'                PopulateTreeView(item.Name)
+'            Catch ex As Exception
+'                'treeViewFolders.Nodes.Item(item.Name).ToolTipText = ex.Message
+'            End Try
+'        Next
+        Dim fbd As New FolderBrowserDialog
+        fbd.Description = "Select a folder to view:"
+        If fbd.ShowDialog = DialogResult.OK Then
+            PopulateTreeView(fbd.SelectedPath)
+        End If
+    End Sub
+    
+    Private Sub PopulateTreeView(path As String) 
+        Dim rootNode As TreeNode
+        
+        Dim info As New DirectoryInfo(path)
+        If info.Exists Then
+            rootNode = New TreeNode(info.Name)
+            rootNode.Tag = info
+            GetDirectories(info.GetDirectories(), rootNode)
+            treeViewFolders.Nodes.Add(rootNode)
+        End If
+    
+    End Sub
+    
+    Private Sub GetDirectories(ByVal subDirs() As DirectoryInfo, ByVal nodeToAddTo As TreeNode)
+    
+        Dim aNode As TreeNode
+        Dim subSubDirs() As DirectoryInfo
+        Dim subDir As DirectoryInfo
+        For Each subDir In subDirs
+            aNode = New TreeNode(subDir.Name, 0, 0)
+            aNode.Tag = subDir
+            aNode.ImageKey = "folder"
+            subSubDirs = subDir.GetDirectories()
+            If subSubDirs.Length <> 0 Then
+                GetDirectories(subSubDirs, aNode)
+            End If
+            nodeToAddTo.Nodes.Add(aNode)
+        Next subDir
+    
+    End Sub
+    
+    Private Sub treeViewFolders_NodeMouseClick(ByVal sender As Object, ByVal e As TreeNodeMouseClickEventArgs) Handles treeViewFolders.NodeMouseClick
+    
+        Dim newSelected As TreeNode = e.Node
+        listViewFiles.Items.Clear()
+        Dim nodeDirInfo As DirectoryInfo = _
+        CType(newSelected.Tag, DirectoryInfo)
+        Dim subItems() As ListViewItem.ListViewSubItem
+        Dim item As ListViewItem = Nothing
+    
+        Dim dir As DirectoryInfo
+        For Each dir In nodeDirInfo.GetDirectories()
+            item = New ListViewItem(dir.Name, 0)
+            subItems = New ListViewItem.ListViewSubItem() _
+                {New ListViewItem.ListViewSubItem(item, "Directory"), _
+                New ListViewItem.ListViewSubItem(item, _
+                dir.LastAccessTime.ToShortDateString())}
+    
+            item.SubItems.AddRange(subItems)
+            listViewFiles.Items.Add(item)
+        Next dir
+        Dim file As FileInfo
+        For Each file In nodeDirInfo.GetFiles()
+            item = New ListViewItem(file.Name, 1)
+            subItems = New ListViewItem.ListViewSubItem() _
+                {New ListViewItem.ListViewSubItem(item, "File"), _
+                New ListViewItem.ListViewSubItem(item, _
+                file.LastAccessTime.ToShortDateString())}
+    
+            item.SubItems.AddRange(subItems)
+            listViewFiles.Items.Add(item)
+        Next file
+    
+        listViewFiles.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize)
+    
     End Sub
 End Class
